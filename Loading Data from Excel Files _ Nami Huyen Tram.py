a734e2445863a7bebae1dfff00f8d6234d8d407b
namihtt <54644654+namihtt@users.noreply.github.com>
2019-09-09 17:15:32 -0400

    Add files via upload

---
+++ Loading Data from Excel Files _ Nami Huyen Tram.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+import pandas as pd 
+
+Location = "POP01.xls"
+df = pd.read_excel(Location)
+
+
+# In[6]:
+
+
+df.head()
+
+
+# In[ ]:
+
+
+
+
