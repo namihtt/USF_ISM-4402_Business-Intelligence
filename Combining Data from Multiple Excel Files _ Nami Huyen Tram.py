a1c01198d6828543845a9f4e2219b07027fa215e
namihtt <54644654+namihtt@users.noreply.github.com>
2019-09-16 22:07:04 -0400

    Add files via upload

---
+++ Combining Data from Multiple Excel Files _ Nami Huyen Tram.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+import pandas as pd
+import numpy as np
+import glob
+
+all_data = pd.DataFrame()
+for f in glob.glob("datasets/weekly_call_data/weekdata*.xlsx"):
+    df = pd.read_excel(f)
+    all_data = all_data.append(df,ignore_index=True)
+    
+all_data.describe()
+
+
+# In[ ]:
+
+
+
+
